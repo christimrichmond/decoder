d140c696df8eeac5ef4ba95904f40118dc7b9f77
christimrichmond <christimrichmond@gmail.com>
2017-12-13 14:41:22 -0500

    encoder decoder project

---
+++ decoder.rb
@@ -0,0 +1,13 @@
+  def initialize(shift, alphabet = ('a'..'z').to_a.join + ('A'..'Z').to_a.join)
+    @alphabet = alphabet
+    @cipher = alphabet.chars.rotate(shift).join
+  end
+
+  def encrypt(text)
+    text.tr(@alphabet, @cipher)
+  end
+
+  def decrypt(text)
+    text.tr(@cipher, @alphabet)
+  end
+end
